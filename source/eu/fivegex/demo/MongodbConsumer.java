a812b5c11a586a716bb09e48cd41d5d6001c1cfc
root <root@DevelVM>
2016-09-14 15:10:05 +0000

    add MongodbConsumer.java

--- source/eu/fivegex/demo/MongodbConsumer.java
+++ source/eu/fivegex/demo/MongodbConsumer.java
@@ -1,40 +1,36 @@
-
-//BasicConsumer.java
-//Author: Stuart Clayman
-//Email: sclayman@ee.ucl.ac.uk
-//Date: Sept 2009
+// MongodbConsumer.java
+// 
 
 package eu.fivegex.demo;
 
 import eu.reservoir.monitoring.core.*;
-import eu.reservoir.monitoring.core.plane.*;
-import eu.reservoir.monitoring.appl.PrintReporter;
-
-import java.io.IOException;
+//import eu.reservoir.monitoring.core.plane.*;
+//import java.io.IOException;
 
 /**
-* A BasicConsumer is an object that is used in application 
-* level code.  It has the necessary functionality to act as a consumer
-* and have plugins for each of the data plane, control plane, and
-* info plane.
-*/
+ * A BasicConsumer is an object that is used in application
+ * level code.  It has the necessary functionality to act as a consumer
+ * and have plugins for each of the data plane, control plane, and
+ * info plane.
+ */
 
 public class MongodbConsumer extends AbstractDataConsumer implements MeasurementReceiver {
 
-	public MongodbConsumer() {
-		// The default way to report a measurement is to print it
-		Reporter reporter =  new PrintReporter();
-	        
-		addReporter(reporter);
-	}
- /**
-  * Construct a MongoDBConsumer.
-  */
-	public MongodbConsumer(String addr, int dataPort) {
-	
-		Reporter reporter =  new MongodbReporter();
-     
-		addReporter(reporter);
-	}
+    /**
+     * Construct a BasicConsumer.
+     */
+    public MongodbConsumer() {
+        // The default way to report a measurement is to print it
+        Reporter reporter =  new PrintReporter();
+
+        addReporter(reporter);
+    }
+
+    public MongodbConsumer(String addr, int dataPort) {
+        Reporter reporter =     new MongodbReporter();
+
+        addReporter(reporter);
+
+    }
 
 }
