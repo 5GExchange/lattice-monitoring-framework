d37f40ce9d1a37e921772944f2d60fd1a4dc0457
paolo.barone@hp.com <paolo.barone@hp.com>
2017-04-07 18:11:09 +0200

    e4

--- deploy-lattice-test.sh
+++ deploy-lattice-test.sh
@@ -6,6 +6,8 @@ RESULT=0
 DDO_IP=docker-orchestrator.ci1.5gex
 echo "installing java (if needed...)"
 ssh ubuntu@$DDO_IP 'sudo apt-get -y install openjdk-8-jre-headless'
+echo "stopping java processes (if any)"
+ssh ubuntu@$DDO_IP 'sudo killall java'
 echo "creating dir..."
 ssh ubuntu@$DDO_IP 'mkdir -p lattice-monitoring-framework'
 scp -r dist/jars ubuntu@$DDO_IP:~/lattice-monitoring-framework 
