32951878dabdb132dd0a87deb884eb205319e6e5
paolo.barone@hp.com <paolo.barone@hp.com>
2017-04-07 17:08:02 +0200

    rt

--- deploy-lattice-test.sh
+++ deploy-lattice-test.sh
@@ -7,10 +7,9 @@ DDO_IP=docker-orchestrator.ci1.5gex
 echo "installing java (if needed...)" >> log.txt
 ssh ubuntu@$DDO_IP 'sudo apt-get -y install openjdk-8-jre-headless'
 echo "creating dir..." >> log.txt
-ssh ubuntu@$DDO_IP 'mkdir -p lattice-monitoring-framework' 
+ssh ubuntu@$DDO_IP 'mkdir -p lattice-monitoring-framework'
+scp -r jars ubuntu@$DDO_IP:~/lattice-monitoring-framework 
+scp -r config ubuntu@$DDO_IP:~/lattice-monitoring-framework 
 ssh ubuntu@$DDO_IP 'cd lattice-monitoring-framework'
-echo "getting jars..." >> log.txt
-ssh ubuntu@$DDO_IP 'wget -r -l1 --no-parent  https://5gex.tmit.bme.hu/jenkins/job/lattice-pb/lastStableBuild/artifact/jars/'
-ssh ubuntu@$DDO_IP 'wget -r -l1 --no-parent  https://5gex.tmit.bme.hu/jenkins/job/lattice-pb/lastStableBuild/artifact/conf/'
 echo "Running..." >> log.txt
 ssh ubuntu@$DDO_IP 'java -cp monitoring-bin-controller.jar eu.fivegex.monitoring.control.controller.Controller controller.properties' 
