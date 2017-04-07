91e2985007db9a29b44200a2e457c90f0ced0800
paolo.barone@hp.com <paolo.barone@hp.com>
2017-04-07 18:06:27 +0200

    Fix conf file

--- deploy-lattice-test.sh
+++ deploy-lattice-test.sh
@@ -4,11 +4,11 @@
 RESULT=0
 
 DDO_IP=docker-orchestrator.ci1.5gex
-echo "installing java (if needed...)" >> log.txt
+echo "installing java (if needed...)"
 ssh ubuntu@$DDO_IP 'sudo apt-get -y install openjdk-8-jre-headless'
-echo "creating dir..." >> log.txt
+echo "creating dir..."
 ssh ubuntu@$DDO_IP 'mkdir -p lattice-monitoring-framework'
 scp -r dist/jars ubuntu@$DDO_IP:~/lattice-monitoring-framework 
 scp -r conf ubuntu@$DDO_IP:~/lattice-monitoring-framework 
-echo "Running..." >> log.txt
-ssh ubuntu@$DDO_IP 'cd lattice-monitoring-framework && java -cp jars/monitoring-bin-controller-0.7.1.jar eu.fivegex.monitoring.control.controller.Controller config/controller.properties'
+echo "Running..."
+ssh ubuntu@$DDO_IP 'cd lattice-monitoring-framework &&  nohup java -cp jars/monitoring-bin-controller-0.7.1.jar eu.fivegex.monitoring.control.controller.Controller conf/controller.properties > lattice.out 2> lattice.out < /dev/null &'
